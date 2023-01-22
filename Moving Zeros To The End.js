e7a036e2e2121c58f84d518655b39ef5a47c9127
ksasrg <ksahtmlcss@gmail.com>
2023-01-22 08:56:07 +0600

    update 7

---
+++ Moving Zeros To The End.js
@@ -0,0 +1,17 @@
+/* DESCRIPTION:
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0] */
+
+
+function moveZeros(arr = []) {
+  for (let i = k = 0; k < arr.length; k++) {
+    if (arr[i] === 0) {      
+      arr.push(arr.splice(i, 1)[0]);
+      i--;
+    } 
+    i++;
+  }
+  
+  return arr;
+}
